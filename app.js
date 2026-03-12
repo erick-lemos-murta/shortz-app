e5a46310856e42376b5046cf1f50ca42c249b8fd
erick-lemos-murta <ericklemos.murta@gmail.com>
2026-03-11 21:43:20 -0300

    modificado a folder user: adicionei userController, mudei o nome de user para userModel e modifiquei o conteúdo; modificado a folder views: Foi adicionado o arquivo login e modificado o register; modificado o arquivo appjs: foi adicionado alguns imports para mensagens de erro e gerenciadores de cookies; modificado a folder routes: o arquivo index foi modificado, adicionando roteadores para login,logout e register, além do feed que ainda não teve seu arquivo criado

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +18,17 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session ({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 *24}
+}));
+app.use(flash());
+app.use((req,res,next)=>{
+  res.locals.messages = req.flash();
+  next();
+});
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,7 +52,7 @@ app.use(function(err, req, res, next) {
 });
 
 const sequelize = require('./config/database');
-const User = require('./modules/user/user');
+const User = require('./modules/user/userModel');
 sequelize.sync({alter:true});//faz as alterações/criações de tabelas dentro do banco
 
 sequelize.authenticate()
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Vídeos Curtos e Engajadores' });
 });
 
 // Requisição GET para formulário de registro
 router.get('/register', function(req,res,next){
-  res.render('register');
+  res.render('register',{title: 'Criar Conta'});
 })
+//Direciona para o userController.js na função register
+router.post('/register', userController.register);
+
+router.get('/login', (req, res) => {
+   res.render('login', { title: 'Entrar' });
+});
+
+// Rota para processar o formulário de login
+router.post('/login', userController.login);
+
+// Rota para processar o logout
+router.post('/logout', userController.logout);
+
+// Rota para exibir o feed de vídeos (protegida por autenticação)
+router.get('/feed', authMiddleware, (req, res) => {
+   res.render('home', { user: req.session.user });
+});
 
 module.exports = router;
